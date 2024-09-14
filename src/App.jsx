b1a61cf893f1b3b05a4c293b65bf33ee69bc017b
Kostia <pancostian@gmail.com>
2024-09-14 16:26:33 -0400

    first-change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import { nanoid } from "nanoid";
 
-import React from "react";
-
-const App = () => {
-  return <div>Hello</div>;
-};
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </>
+  );
+}
 
 export default App;
