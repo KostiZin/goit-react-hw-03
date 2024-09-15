0b62ba621ba1f375809cc5657b8e0aee53a5c603
Kostia Zinchenko <pancostian@gmail.com>
2024-09-14 21:15:56 -0400

    third-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import contacts from "./components/contacts.json";
+import data from "./components/contacts.json";
 
 function App() {
-  const initialValues = {
-    name: "Kostia",
-    number: 876867,
-  };
-  console.log({ contacts });
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue);
+
+  const [users, setUsers] = useState(data);
+
+  useEffect(() => {
+    const handleFilter = data.filter((user) =>
+      user.name.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    setUsers(handleFilter);
+  }, [inputValue]);
+
+  console.log(users);
 
   return (
     <>
       <div>
         <h1>Phonebook</h1>
-        <ContactForm values={initialValues} />
-        <SearchBox />
-        <ContactList contacts={contacts} />
+        {/* <ContactForm values={initialValues} /> */}
+        <SearchBox data={inputValue} setData={setInputValue} />
+
+        <p>{inputValue}</p>
+        <ContactList contacts={users} />
       </div>
     </>
   );
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import css from "./SearchBox.module.css";
 
-const SearchBox = () => {
-  return <div></div>;
+const SearchBox = ({ data, setData }) => {
+  const handleChange = (e) => {
+    setData(e.target.value);
+  };
+
+  return (
+    <div className={css.container}>
+      Find contacts by name
+      <input type="text" value={data} onChange={handleChange} />
+    </div>
+  );
 };
 
 export default SearchBox;
