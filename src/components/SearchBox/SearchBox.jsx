1efaa1a9aa8490606805ca6f1457114eb938ccaa
Kostia Zinchenko <pancostian@gmail.com>
2024-09-17 19:20:05 -0400

    sixth-commit

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,28 +1,16 @@
 import css from "./SearchBox.module.css";
 
-// const SearchBox = ({ data, setData }) => {
-//   const handleChange = (e) => {
-//     setData(e.target.value);
-//   };
-
-//   return (
-//     <div className={css.container}>
-//       Find contacts by name
-//       <input type="text" value={data} onChange={handleChange} />
-//     </div>
-//   );
-// };
-
 // eslint-disable-next-line react/prop-types
 const SearchBox = ({ data, setData }) => {
-  // const handleChange = (e) => {
-  //   setData(e.target.value);
-  // };
-
   return (
     <div className={css.container}>
-      Find contacts by name
-      <input type="text" value={data} onChange={setData} />
+      <p>Find contacts by name</p>
+      <input
+        className={css.input}
+        type="text"
+        value={data}
+        onChange={setData}
+      />
     </div>
   );
 };
